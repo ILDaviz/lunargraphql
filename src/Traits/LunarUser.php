af4291a9e42ba37d8feab4f20c8f34ee00a2b050
David Galet <david.galet@offdigit.com>
2023-11-19 19:18:46 +0100

    ✨ Add trait for LunarUser.php

---
+++ src/Traits/LunarUser.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Lunargraphql\Traits;
+
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Lunar\Models\Customer;
+use Lunar\Models\Order;
+
+trait LunarUser
+{
+    public function customers(): BelongsToMany
+    {
+        $prefix = config('lunar.database.table_prefix');
+
+        return $this->belongsToMany(Customer::class, "{$prefix}customer_user");
+    }
+
+    public function latestCustomer()
+    {
+        return $this->customers()->orderBy('created_at', 'desc')->orderBy('id', 'desc')->first();
+    }
+
+    /**
+     * Return the user orders relationship.
+     */
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
+}
