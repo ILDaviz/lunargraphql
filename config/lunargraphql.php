288e2ba33b1d10db96e0f8f135fd27ad72eaa95d
David Galet <david.galet@offdigit.com>
2023-11-22 00:23:51 +0100

    🔧 Update config

--- config/lunargraphql.php
+++ config/lunargraphql.php
@@ -1,5 +1,15 @@
 <?php
 
 return [
-    //
+    /**
+     * This is a model of the user that will be used for authentication.
+     * Default: App\Models\User
+     */
+    'user_model' => 'App\Models\User',
+
+    /**
+     * This is a provider of the user that will be used for reset password.
+     * Default: users
+     */
+    'user_auth_provider' => 'users',
 ];
